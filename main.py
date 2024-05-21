593f5d4d90858b8fff82f3baf881a35e7043d256
Asadullo <hayrullayevasadullo@gmail.com>
2024-05-21 19:46:02 +0500

    homework

--- main.py
+++ main.py
@@ -1,41 +1,54 @@
-#1 operators > n <
-# tst = int(input('gimme number'))
-# if tst >0:
-#     print("True")
-# else:
-#     print("false")
-#
-#
-# if tst > 10:
-#     print("tst больше 10")
-# else:
-#     print("tst меньше или равно 10")
+                         #Homework
+class A :
+    def add(self,a,b):
+        return a + b
+    def minus(self,a,b):
+        return a - b
+    def mult(self,a,b):
+        return a * b
+    def div(self,a,b):
+        return a/b
 
-tst = 15
-if tst > 10 :
-    if tst <20:
-        print("True")
-else :
-    print("nigga")
+    @staticmethod
+    def frst(self,a,b):
+        return a+b
+    @staticmethod
+    def nqr(a,b):
+       return a**b
+cal = A()
 
-tst = -5
-if 0 > tst < -10 :
-    if 30 > tst < -8:
-        print("nigga")
-else:
-    print("lll")
+def input_ask():
+    x = int(input('ur first number : '))
+    y = int(input('ur second number : '))
+    return x,y
+symbol = input(" choose (+ - / * # & **)")
 
-tst = ['a', 'b', 'c', 'd','e']
-if 6> len(tst) < 0:
-    print('hhh')
-else:
-    print("miraf is gay ")
+if symbol in ['+','-','*','/','&','**']:
+    num1,num2 = input_ask()
 
-# git checkout -b name_branch  ## creatin new branch
+    # if symbol == '+':
+    #     num1,num2 = input_ask()
+    if symbol == '+':
+        print("Result : ", cal.add(num1,num2))
+    # if symbol== '-':
+    #     num1,num2 = input_ask()
+    if symbol == '-':
+        print("Result : ", cal.minus(num1,num2))
+    # if symbol== '*':
+    #     num1,num2 = input_ask()
+    if symbol == '*':
+        print("Result : ", cal.mult(num1,num2))
+    # if symbol== '/':
+    #     num1,num2 = input_ask()
+    if symbol == '/':
+        print("Result : ", cal.div(num1,num2))
+    if symbol == '**':
+        zza = input("nigga pay $50 first , will you pay? ")
+        if zza == 'yes':
+            dda = input('confirm ur payment : ' )
+            if dda >= '50':
+                print('thank you there is ur result : ', cal.nqr(num1,num2))
+    if symbol == '&':
+        print(input('nigga pay $120 first'))
 
-# nigga is criminal he stole that girls purse and kidnapped her
-# after all of this shit he raped her in his basement
-# after about 2 years he married her at nearest church
-# now that nigga got shot by policeman
-# that is how nigga things are done
-# P.S he loved KFC
+
